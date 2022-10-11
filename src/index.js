c62f86f1a248049f602ac5c050e041d2fd615ff9
Alicenanguo <106508822+Alicenanguo@users.noreply.github.com>
2022-10-11 10:06:51 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {Provider} from 'react-redux';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+import configureStore from './store';
+import './index.css'
+
+const store = configureStore();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <BrowserRouter>
+        <App />
+        </BrowserRouter>
+        </Provider>      
+  </React.StrictMode>,
+  document.getElementById('root')
+);
